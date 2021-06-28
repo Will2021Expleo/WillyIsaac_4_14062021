3402958d4f1900bb1174c82e42d7f3a79a80dab9
Will2021Expleo <willy.isaac@expleogroup.com>
2021-06-28 17:53:52 +0200

    fermeture du modal avec la croix et message après validation du formulaire

--- modal.js
+++ modal.js
@@ -12,12 +12,38 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
-function launchModal() {
+function launchModal(e) {
+  e.preventDefault()
   modalbg.style.display = "block";
+
+  modalbg.addEventListener("click", closeModal);
+  modalbg.querySelector(".close").addEventListener('click', closeModal);
+  modalbg.querySelector(".content").addEventListener('click', stopPropagation);
 }
 
+// close modal
+function closeModal(e) {
+  if (modalbg === null) return
+  e.preventDefault()
+  modalbg.style.display = "none"
 
+  modalbg.removeEventListener('click', closeModal);
+  modalbg.querySelector(".close").removeEventListener("click", closeModal);
+  modalbg.querySelector(".content").removeEventListener("click", stopPropagation);
+}
+
+const stopPropagation = function(e) {
+  e.stopPropagation()
+}
+
+document.forms["reserve"].addEventListener("submit", function(e) {
+  e.preventDefault()
+  alert('Bravo! Inscription validée')
+})
